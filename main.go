cbfc19d2be161e7d9ac580edcaaa73dfb2e26e71
Chris Ash <goodvibesjumper@hotmail.com>
2019-09-08 12:42:27 -0600

    working version

--- main.go
+++ main.go
@@ -7,22 +7,23 @@ import (
 
 func main() {
 
-	words := []string{"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"}
+	words := []string{"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog", "nifty!"}
 	k := 16
 
-	currentGroup := []string{words[0]}
-	cnt := len(words[0]) // # of letters in the current group build
+	// 1) Build arrays of word groupings for each line
 	groupings := [][]string{}
+	currentGroup := []string{words[0]}
+	letterCount := len(words[0]) // # of letters in the current group build
 
 	for x := 1; x < len(words); x++ {
-		if len(words[x])+1+cnt > k {
+		if len(words[x])+1+letterCount > k {
 			// we are starting a new group
 			groupings = append(groupings, currentGroup)
 			fmt.Println(currentGroup)
 			currentGroup = []string{}
-			cnt = 0
+			letterCount = 0
 		}
-		cnt += len(words[x]) + 1
+		letterCount += len(words[x]) + 1
 		currentGroup = append(currentGroup, words[x])
 	}
 
@@ -31,18 +32,30 @@ func main() {
 		groupings = append(groupings, currentGroup)
 	}
 
-	//////////// now we have groupings
+	// have getGroupString() process each line and put them in results
 	fmt.Println("-------------------")
 	fmt.Println(groupings)
 
+	results := []string{}
 	for x := 0; x < len(groupings); x++ {
-		getGroupString(groupings[x], k)
+		results = append(results, getGroupString(groupings[x], k))
 	}
+
+	fmt.Println(strings.Repeat("*", k+4))
+	for x := 0; x < len(results); x++ {
+		fmt.Printf("* %v *\n", results[x])
+	}
+	fmt.Printf(strings.Repeat("*", k+4))
 
 }
 
-func getGroupString(sgroup []string, k int) {
+func getGroupString(sgroup []string, k int) string {
 	fmt.Printf("Welcome to getGroupString()  sgroup=%v  k=%v\n", sgroup, k)
+
+	if len(sgroup) == 1 {
+		ret := sgroup[0] + strings.Repeat(" ", k-len(sgroup[0]))
+		return ret
+	}
 
 	gaps := len(sgroup) - 1
 	letterCount := 0
@@ -55,34 +68,31 @@ func getGroupString(sgroup []string, k int) {
 	spaceCount := k - letterCount
 	fmt.Printf("spaces=%d\n", spaceCount)
 
-	gap_spc := make([]int, gaps)
+	gapSpc := make([]int, gaps)
 
 	if spaceCount > gaps {
-		for x := 0; x < len(gap_spc); x++ {
-			gap_spc[x] = spaceCount / gaps
+		for x := 0; x < len(gapSpc); x++ {
+			gapSpc[x] = spaceCount / gaps
 		}
 	}
 
 	i := 0
 	for x := spaceCount % gaps; x > 0; x-- {
-		gap_spc[i]++
+		gapSpc[i]++
 		i++
 	}
 
-	fmt.Printf("gap_spc[] = %v\n", gap_spc)
+	fmt.Printf("gapSpc[] = %v\n", gapSpc)
 
 	ret := ""
 	ret += sgroup[0]
 
-	for x := 0; x < len(gap_spc); x++ {
-		ret += strings.Repeat(" ", gap_spc[x])
+	for x := 0; x < len(gapSpc); x++ {
+		ret += strings.Repeat(" ", gapSpc[x])
 		ret += sgroup[x+1]
 	}
 
-	if len(sgroup) > 1 {
-		ret += sgroup[len(sgroup)-1]
-	}
-
 	fmt.Printf("ret = %v\n", ret)
+	return ret
 
 }
